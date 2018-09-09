59d71d9d965c97acb47cdc07146ffb1fbe6ab78f
Nguyen Thanh Duc <ntduc2812@gmail.com>
2018-09-09 21:55:25 +0700

    ss

--- src/main/java/com/github/demo/DucController.java
+++ src/main/java/com/github/demo/DucController.java
@@ -1,7 +1,12 @@
 package com.github.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class DucController {
-    public String showName(){
-        return "My name is Duc";
-    }
+    @Autowired DucService ducService;
+
 }
---
+++ src/main/java/com/github/demo/DucService.java
@@ -0,0 +1,10 @@
+package com.github.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DucService {
+    public String getName(){
+        return "My name is Duc Nguyen";
+    }
+}
